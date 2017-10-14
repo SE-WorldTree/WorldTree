b81353cbde6514c3fd4417f32441f3a0f3054eb1
dimitriwhy <en_why@live.cn>
2017-10-14 10:05:22 +0800

    add django framework and user management app

---
+++ userManagement/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django import forms
+from userManagement.models import User
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label='姓名', max_length=100)
+    email = forms.CharField(label="邮箱", max_lengtj=100)
+    password = forms.CharField(label='密码', widget=forms.PasswordInput())
+
+
+def register(request):
+    if request.method == 'POST':
+        uf = UserForm(request.POST)
+        if uf.is_valid():
+            # 获得表单数据
+            username = uf.cleaned_data['username']
+            email = uf.cleaned_data['email']
+            password = uf.cleaned_data['password']
+            # 添加到数据库
+            User.objects.create(username=username, email=email, password=password)
+            return HttpResponse('register success!!')
+    else:
+        uf = UserForm()
+    return render_to_response('register.html', {'uf': uf}, context_instance=RequestContext(request))
+
+
+def login(request):
+    if request.method == 'POST':
+        uf = UserForm(request.POST)
+        if uf.is_valid():
+            # 获取表单用户密码
+            email = uf.cleaned_data['email']
+            password = uf.cleaned_data['password']
+            # 获取的表单数据与数据库进行比较
+            user = User.objects.filter(email__exact=email, password__exact=password)
+            if user:
+                # 比较成功，跳转index
+                response = HttpResponseRedirect('/userManagement/index/')
+                # 将username写入浏览器cookie,失效时间为3600
+                response.set_cookie('username', email, 3600)
+                return response
+            else:
+                # 比较失败，还在login
+                return HttpResponseRedirect('/userManagement/login/')
+    else:
+        uf = UserForm()
+    return render_to_response('login.html', {'uf': uf}, context_instance=RequestContext(req))
+
+
+def index(request):
+    email = request.COOKIES.get('email', '')
+    return render_to_response('index.html', {'email': email})
+
+
+def logout(request):
+    response = HttpResponse('logout !!')
+    # 清理cookie里保存username
+    response.delete_cookie('email')
+    return response
