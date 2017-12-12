e07cb03982cefb9d2c863b64cfcdd4fb2d2a9d05
holynova-SD <holyalakir@outlook.com>
2017-12-12 08:34:53 +0800

    feat: add message page

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^balabala/', views.rejson, name='rejson'),
     url(r'^relation/', views.uuuu, name='relation'),
     url(r'^profile/', views.nnnn, name='profile'),
+    url(r'^message/', views.message, name='message'),
     url(r'^hhhh/', views.hhhh, name='hhhh'),
     url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name="user_active")
 ]
